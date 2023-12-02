8557be5116a691c97d4bf4e7a0a0fdfc00289bb2
yys <dgh06008@naver.com>
2023-12-02 22:51:47 +0900

    23.12.02

---
+++ src/yalc_java/sec05/chap01/ex01/main.java
@@ -0,0 +1,13 @@
+package yalc_java.sec05.chap01.ex01;
+
+public class main {
+    public static void main(String[] args) {
+        Button button1 = new Button('1', 1, "DARK");
+        Button buttonPlus = new Button('+', 3, "DARK");
+        Button buttonClear = new Button('C', 2, "DARK");
+
+        button1.place();
+        buttonPlus.place();
+        buttonClear.place();
+    }
+}
